bf2ebb228470db4da8f5ca7b2fdaa246a6027e48
Vladimir Simankin <man4ik163@gmail.com>
2020-09-19 11:31:23 +0300

    feature/AM-0 initial commit

---
+++ src/main/java/ru/simankin/aboutmoney/AboutMoneyApplication.java
@@ -0,0 +1,13 @@
+package ru.simankin.aboutmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AboutMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AboutMoneyApplication.class, args);
+	}
+
+}
